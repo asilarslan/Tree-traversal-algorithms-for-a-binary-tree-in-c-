48e2b054e9823f4ef3e45995989f214f115e5746
Asil Arslan <asilarslan93@gmail.com>
2014-07-09 18:54:44 +0300

    Tree traversal algorithms for a binary tree
    1 Preorder
    2 In-order
    3 Post-order

---
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree<Type> where Type : IComparable<Type>
+    {
+        public Node<Type> root;
+
+        internal BinaryTree()
+        {
+            root = null;
+        }
+
+        public virtual bool recursiveInsert(Type value)
+        {
+            try
+            {
+                return recursiveInsertNode(value, root);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        protected virtual bool recursiveInsertNode(Type value, Node<Type> curNode = null, Node<Type> parentNode = null)
+        {
+            try
+            {
+                if (curNode == null)
+                {
+                    if (curNode == root)
+                        root = new Node<Type>(value);
+                    else
+                    {
+                        curNode = new Node<Type>(value);
+                        curNode.parent = parentNode;
+                        if (parentNode != null)
+                        {
+                            if (parentNode.value.CompareTo(value) > 0) parentNode.left = curNode;
+                            else parentNode.right = curNode;
+                        }
+                    }
+                }
+                else if (curNode.value.CompareTo(value) > 0) recursiveInsertNode(value, curNode.left, curNode);
+                else if (curNode.value.CompareTo(value) == 0) return false;
+                else recursiveInsertNode(value, curNode.right, curNode);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        
+        public StringBuilder str = new StringBuilder();
+        public virtual string preOrderTraversal(Node<Type> node)
+        {
+            if (node != null)
+            {
+                Console.Write( node + ",");
+                preOrderTraversal(node.left);
+                preOrderTraversal(node.right);
+            }
+            return str.ToString();
+        }
+        public virtual string inOrderTraversal(Node<Type> node)
+        {
+            if (node != null)
+            {
+                
+                inOrderTraversal(node.left);
+                Console.Write(node + ",");
+                inOrderTraversal(node.right);
+            }
+            return str.ToString();
+        }
+        public virtual string postOrderTraversal(Node<Type> node)
+        {
+            if (node != null)
+            {
+                postOrderTraversal(node.left);
+                postOrderTraversal(node.right);
+                Console.Write(node + ",");
+            }
+            return str.ToString();
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//txt write: http://msdn.microsoft.com/tr-tr/library/8bh11f1k.aspx
+namespace BinaryTree
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string fileName = "../../" + "hw" + ".txt";
+            //creating the file
+            FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+            Console.WriteLine("hw.txt created");
+            fs.Close();
+
+            StreamWriter sw = new StreamWriter(fileName);
+            List<int> list = new List<int>();
+            int val;
+            Random r;
+            int IntialCount = 1;
+            int count = 300;
+            int maxRandomValue = 1000;
+
+            BinaryTree<int> bt = new BinaryTree<int>();
+            while (IntialCount <= count)
+            {
+                r = new Random();
+                val = r.Next(maxRandomValue);
+                if (!list.Contains(val))
+                {
+                    bt.recursiveInsert(val);
+                    list.Add(val);
+                    sw.Write(val + " ");
+                    IntialCount++;
+                }
+
+            }
+            sw.Close();
+            string[] the_array = list.Select(i => i.ToString()).ToArray();
+
+            string[] lines2 = System.IO.File.ReadAllLines("../../hw.txt");
+            // Display the file contents by using a foreach loop.
+            System.Console.Write("\n\n\nIN-ORDER of hw.txt = ");
+
+
+            foreach (string line2 in the_array)
+            {
+                // Use a tab to indent each line of the file.
+                bt.inOrderTraversal(bt.root);
+                break;
+            }
+
+            System.Console.Write("\n\n\nPRE-ORDER of hw.txt = ");
+
+
+            foreach (string line2 in the_array)
+            {
+                // Use a tab to indent each line of the file.
+                bt.preOrderTraversal(bt.root);
+                break;
+            }
+
+            System.Console.Write("\n\n\nPOST-ORDER of hw.txt = ");
+
+
+            foreach (string line2 in the_array)
+            {
+                // Use a tab to indent each line of the file.
+                bt.postOrderTraversal(bt.root);
+                break;
+            }
+            System.Console.ReadKey();
+        }
+    }
+
+}
